fb55c9b1d8fa10f8f721c590bd38c720fb7e011d
pratap <pratap477@gmail.com>
2015-09-08 00:22:14 +0530

    addd sop

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   get 'homes/penalties' => 'homes#penalties'
   get 'homes/legalprovisions' => 'homes#legalprovisions'
   get 'homes/document' => 'homes#document'
+  get 'homes/sop' => 'homes#sop'
   resources :complaints
 
   resources :reports do
